2de5ecd12c6d4fd18e1bf59a94a1d6b86a4822c3
Phil Martinez <phil@philsquare.com>
2013-08-11 16:47:07 -0500

    Initial commit

---
+++ theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Bootstrap_Slim extends Theme {
+
+    public $name = 'Bootstrap Slim';
+    public $author = 'Philsquare';
+    public $author_website = 'http://philsquare.com';
+    public $website = 'http://philsquarelabs.com/pyrocms/themes/bootstrap-slim';
+    public $description = 'Basic theme using Twitter Bootstrap';
+    public $version = '0.9.0';
+
+}
+
+/* End of file theme.php */
