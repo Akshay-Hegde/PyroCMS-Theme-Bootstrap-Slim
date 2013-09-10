a8dcdff85b12c50b18bdb6a4010367aa27d82b08
Phil Martinez <phil@philsquare.com>
2013-09-10 13:52:58 -0500

    styled login and register

--- views/modules/users/register.php
+++ views/modules/users/register.php
@@ -1,48 +1,48 @@
-<h2 class="page-title" id="page_title"><?php echo lang('user:register_header') ?></h2>
-
-<p>
-	<span id="active_step"><?php echo lang('user:register_step1') ?></span> -&gt;
-	<span><?php echo lang('user:register_step2') ?></span>
-</p>
+<h1><?php echo lang('user:register_header') ?></h1>
 
 <?php if ( ! empty($error_string)):?>
 <!-- Woops... -->
-<div class="error-box">
+<div class="alert alert-danger">
 	<?php echo $error_string;?>
 </div>
 <?php endif;?>
 
-<?php echo form_open('register', array('id' => 'register')) ?>
-<ul>
+<?php echo form_open('register', array('id' => 'register'), 'role="form"') ?>
+
 	
 	<?php if ( ! Settings::get('auto_username')): ?>
-	<li>
+	<div class="form-group">
 		<label for="username"><?php echo lang('user:username') ?></label>
-		<input type="text" name="username" maxlength="100" value="<?php echo $_user->username ?>" />
-	</li>
+		<input type="text" name="username" maxlength="100" value="<?php echo $_user->username ?>" class="form-control">
+	</div>
 	<?php endif ?>
 	
-	<li>
+	<div class="form-group">
 		<label for="email"><?php echo lang('global:email') ?></label>
-		<input type="text" name="email" maxlength="100" value="<?php echo $_user->email ?>" />
+		<input type="text" name="email" maxlength="100" value="<?php echo $_user->email ?>" class="form-control">
 		<?php echo form_input('d0ntf1llth1s1n', ' ', 'class="default-form" style="display:none"') ?>
-	</li>
+	</div>
 	
-	<li>
+	<div class="form-group">
 		<label for="password"><?php echo lang('global:password') ?></label>
-		<input type="password" name="password" maxlength="100" />
-	</li>
-
-	<?php foreach($profile_fields as $field) { if($field['required'] and $field['field_slug'] != 'display_name') { ?>
-	<li>
-		<label for="<?php echo $field['field_slug'] ?>"><?php echo (lang($field['field_name'])) ? lang($field['field_name']) : $field['field_name'];  ?></label>
-		<div class="input"><?php echo $field['input'] ?></div>
-	</li>
-	<?php } } ?>
-
+		<input type="password" name="password" maxlength="100" class="form-control">
+	</div>
 	
-	<li>
-		<?php echo form_submit('btnSubmit', lang('user:register_btn')) ?>
-	</li>
-</ul>
+	<div id="profile-fields">
+
+		<?php foreach($profile_fields as $field): ?>
+
+			<?php if($field['required'] and $field['field_slug'] != 'display_name'): ?>
+				<div class="form-group">
+					<label for="<?php echo $field['field_slug'] ?>"><?php echo (lang($field['field_name'])) ? lang($field['field_name']) : $field['field_name'];  ?></label>
+					<?php echo $field['input'] ?>
+				</div>
+			<?php endif; ?>
+			
+		<?php endforeach; ?>
+
+	</div>
+
+	<?php echo form_submit('btnSubmit', lang('user:register_btn'), 'class="btn btn-default"') ?>
+
 <?php echo form_close() ?>
