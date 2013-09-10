d1e64b7a3ec2586e18fd4895c14700f807474655
Phil Martinez <phil@philsquare.com>
2013-09-10 12:47:13 -0500

    updated comments and comment form

--- views/modules/comments/display.php
+++ views/modules/comments/display.php
@@ -2,26 +2,23 @@
 	
 	<?php foreach ($comments as $item): ?>
 		
-		<div class="comment">
-			<div class="image">
+		<div class="media well">
+			<div class="pull-left">
 				<?php echo gravatar($item->user_email, 60) ?>
 			</div>
-			<div class="details">
-				<div class="name">
-					<?php echo $item->user_name ?>
-				</div>
+			<div class="media-body">
+				<h4 class="media-heading"><?php echo $item->user_name ?></h4>
 				<div class="date">
 					<p><?php echo format_date($item->created_on) ?></p>
 				</div>
-				<div class="content">
-					<?php if (Settings::get('comment_markdown') and $item->parsed): ?>
-						<?php echo $item->parsed ?>
-					<?php else: ?>
-						<p><?php echo nl2br($item->comment) ?></p>
-					<?php endif ?>
-				</div>
+				<?php if (Settings::get('comment_markdown') and $item->parsed): ?>
+					<?php echo $item->parsed ?>
+				<?php else: ?>
+					<p><?php echo nl2br($item->comment) ?></p>
+				<?php endif ?>
 			</div>
-		</div><!-- close .comment -->
+		</div>
+
 	<?php endforeach ?>
 	
 <?php else: ?>
